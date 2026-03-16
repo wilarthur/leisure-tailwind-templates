c0713a5ba7470bdc36030949878a019fe82d4e2d
kintyre-creative <wil@kintyrecreative.co.uk>
2026-03-16 10:57:03 +0000

    Text with image section

--- dist/script.js
+++ dist/script.js
@@ -4,5 +4,5 @@ function toggleMenu() {
   const mobileMenu = document.getElementById("mobile-menu");
   const htmlTag = document.documentElement;
   mobileMenu.classList.toggle("translate-x-full");
-  //   htmlTag.classList.toggle("overflow-hidden");
+  htmlTag.classList.toggle("overflow-y-hidden");
 }
