d7b74a5361d9675693a9d5644cb4c901a5dd27fb
kintyre-creative <wil@kintyrecreative.co.uk>
2026-03-16 10:07:20 +0000

    Overflow X hidden, mobile script added to script tag

---
+++ dist/script.js
@@ -0,0 +1,8 @@
+/* Mobile menu */
+
+function toggleMenu() {
+  const mobileMenu = document.getElementById("mobile-menu");
+  const htmlTag = document.documentElement;
+  mobileMenu.classList.toggle("translate-x-full");
+  //   htmlTag.classList.toggle("overflow-hidden");
+}
