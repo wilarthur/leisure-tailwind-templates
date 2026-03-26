0e6d62023d60fb98778c7993bb6629af5fa7da3f
kintyre-creative <wil@kintyrecreative.co.uk>
2026-03-26 09:03:43 +0000

    Mobile menu overflow fixed

--- dist/script.js
+++ dist/script.js
@@ -3,9 +3,18 @@
 function toggleMenu() {
   const mobileMenu = document.getElementById("mobile-menu");
   const htmlTag = document.documentElement;
-  mobileMenu.classList.toggle("hidden");
-  setTimeout(function () {
+
+  if (mobileMenu.classList.contains("hidden")) {
+    mobileMenu.classList.toggle("hidden");
+    setTimeout(function () {
+      mobileMenu.classList.toggle("translate-x-full");
+      htmlTag.classList.toggle("overflow-y-hidden");
+    }, 1);
+  } else {
     mobileMenu.classList.toggle("translate-x-full");
     htmlTag.classList.toggle("overflow-y-hidden");
-  }, 1);
+    setTimeout(function () {
+      mobileMenu.classList.toggle("hidden");
+    }, 500);
+  }
 }
